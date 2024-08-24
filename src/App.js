a41b97c02f072270c0ad322b1d377d6d96ffa75b
dawitmanukian <davit.manukyan.d@tumo.org>
2024-08-25 03:54:22 +0400

    updt

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loading from "./pages/loading/Loading";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import { useState } from "react";
 
 function App() {
+  const [showLoading, setShowLoading] = useState(true);
+
+  setTimeout(() => setShowLoading(false), "3000")
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-        </Routes>
-        <Navbar />
-      </Router>
+      {showLoading ? (
+        <Loading />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/*" element={<Home />} />
+          </Routes>
+          <Navbar />
+        </Router>
+      )}
     </div>
   );
 }
